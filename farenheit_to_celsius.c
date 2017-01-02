05af1afd645fb2371a2a34a7aca6ae6488ed232b
Cloyd Karlo C. Castillo <karencastillo18@users.noreply.github.com>
2017-01-03 07:57:07 +0800

    Update and rename hello_world.c to farenheit_to_celsius.c

---
+++ farenheit_to_celsius.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+	int main (){
+		float F, Cel;
+		
+		printf("Enter the value of fahrenheit\n");
+		scanf("&f", &F);
+		
+		printf("\in celcius value is");
+		printf("&f", Cel = (F-32)*5/9);
+			
+	}
