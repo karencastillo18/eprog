2be86e563d6f1a6caa887c8bf1bfb49c0a92071f
Cloyd Karlo C. Castillo <karencastillo18@users.noreply.github.com>
2017-01-03 07:58:58 +0800

    Create if_play.c

---
+++ if_play.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int grade;
+	
+	 printf("Enter grade:");
+	 scanf("%d" , &grade);
+	 printf("Grade is: %d", grade);
+	 if(grade%3==0){
+	 	printf("Ed");
+	 }
+	    if(grade%5==0){
+		
+	 	printf("pogi");
+	    }
+	    if(grade%5==0 && 3==0){
+	    	printf(" Ed pogi");
+		}
+}
+	 
+	  
